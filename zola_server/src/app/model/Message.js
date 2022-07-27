c9255fb9cc9bf756e38b9d585fdd8c32cb7ac454
nxquan <quananhb2410@gmail.com>
2022-07-28 01:17:21 +0700

    Fix sended time of message v2

--- zola_server/src/app/model/Message.js
+++ zola_server/src/app/model/Message.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose');
 
-const Message = new mongoose.Schema({
-	message: {
-		text: {
-			type: String,
+const Message = new mongoose.Schema(
+	{
+		message: {
+			text: {
+				type: String,
+				required: true,
+			},
+		},
+		users: Array,
+		sender: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
 			required: true,
 		},
-	},
-	users: Array,
-	sender: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
 	},
 	{
 		timestamps: true,
 	}
-});
+);
 
 module.exports = mongoose.model('Message', Message);
